1a4c20318776343abb2410cc81c6e4110b539314
kaalid arare <jaliddd11@gmail.com>
2025-01-06 17:21:44 -0800

    listpractice complete

--- src/ListPractice.java
+++ src/ListPractice.java
@@ -1,28 +1,40 @@
+import java.util.*;
+
 public class ListPractice {
 
 
   public static void main(String[] args) {
     // Create an empty ArrayList of Strings and assign it to a variable of type List
-
+    ArrayList<String> arr = new ArrayList<>();
     // Add 3 elements to the list (OK to do one-by-one)
-
+    arr.add("john");
+    arr.add("kaalid");
+    arr.add("tyler");
     // Print the element at index 1
-
+    System.out.println(arr.get(0));
     // Replace the element at index 1 with a new value
     // (Do not insert a new value. The length of the list should not change)
-
+    arr.set(0,"david");
+    System.out.println(arr.get(0));
     // Insert a new element at index 0 (the length of the list will change)
-
+    arr.add(0, "liban");
+    System.out.println(arr.get(0));
     // Check whether the list contains a certain string
-
+    System.out.println(arr.contains("david"));
+    System.out.println();
     // Iterate over the list using a traditional for-loop.
     // Print each index and value on a separate line
-
+    for(int i = 0; i < arr.size(); i++) {
+      System.out.println(arr.get(i));
+    }
     // Sort the list using the Collections library
-
+    Collections.sort(arr);
     // Iterate over the list using a for-each loop
     // Print each value on a second line
-
+    System.out.println();
+    for(int i = 0; i < arr.size(); i++) {
+      System.out.println(arr.get(i));
+    }
     /*
      * Usage tip!
      * 
